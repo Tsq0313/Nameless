a091ebab6124931987ae8b60c432d36ffccf963c
Tsq031397 <59818719+Tsq031397@users.noreply.github.com>
2020-01-14 15:05:15 +0800

    fix max length size error

--- preprocess.py
+++ preprocess.py
@@ -43,7 +43,11 @@ with open(lines_file, "r") as f:
             m_lines.append(v.lower())
 
 # Load Pre-Trained BERT Model via TF 2.0
-max_seq_length = 128  # Your choice here.
+# Prepare Data
+lines = m_lines + f_lines
+size = len(lines)
+max_seq_length = 130
+
 input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
                                        name="input_word_ids")
 input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
@@ -58,11 +62,6 @@ model = Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=[pooled_
 vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
 do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
 tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
-
-# Prepare Data
-lines = m_lines + f_lines
-size = len(lines)
-max_seq_length = 128
 
 def get_masks(tokens, max_seq_length):
     """Mask for padding"""
